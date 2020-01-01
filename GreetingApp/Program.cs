47daf115cb9d2910222e1744f2995da9ce7881aa
theadambyrne <adam.r.byrne@gmail.com>
2020-01-01 21:40:42 +0000

    adding from workspace in VS

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        private const string nameQ = "What is your name?";
+        private const string ageQ = "What is your age?";
+        private const string placeQ = "What are you from?";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nameQ);
+            string name = Console.ReadLine();
+
+            Console.WriteLine(ageQ);
+            string age = Console.ReadLine();
+
+            Console.WriteLine(placeQ);
+            string place = Console.ReadLine();
+
+            Console.WriteLine("Hello, "+name+"!  You are "+age+", "+"from "+place+"!");
+        }
+    }
+}
